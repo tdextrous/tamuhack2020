cf5e8df54cb2d2458dc9dc84b29f3452a5e943dc
Thomas Dexter <t.dextrous@gmail.com>
2020-01-26 12:48:07 -0600

    change api endpoint

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export const API_ENDPOINT = 'http://ec2-54-186-192-110.us-west-2.compute.amazonaws.com'; //process.env.API_ENDPOINT || 'http://localhost:3030';
+export const API_ENDPOINT = 'https://api.th2020.thomasjdexter.com'; //process.env.API_ENDPOINT || 'http://localhost:3030';
