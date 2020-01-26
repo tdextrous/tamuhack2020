87ca69e94e94c20e08bbb6128b5ce87e19b27c14
Thomas Dexter <t.dextrous@gmail.com>
2020-01-26 10:15:45 -0600

    Production stuff

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export const API_ENDPOINT = process.env.API_ENDPOINT || 'http://localhost:3030';
+export const API_ENDPOINT = 'http://ec2-54-186-192-110.us-west-2.compute.amazonaws.com'; //process.env.API_ENDPOINT || 'http://localhost:3030';
